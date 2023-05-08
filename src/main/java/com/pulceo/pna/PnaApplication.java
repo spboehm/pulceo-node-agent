752692d58462dd3aa977aac13bcab40075938e05
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-05-08 22:05:30 +0200

    initial commit of pulceo-node-agent

---
+++ src/main/java/com/pulceo/pna/PnaApplication.java
@@ -0,0 +1,13 @@
+package com.pulceo.pna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulceo/pna/PnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulceo.pna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

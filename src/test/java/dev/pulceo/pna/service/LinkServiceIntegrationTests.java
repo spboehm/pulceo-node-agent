7aab5ea126e57bc28d960b1a472ca66bc5d0cec9
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-11-10 10:50:23 +0100

    add tests for npingservice and pingservice

--- src/test/java/dev/pulceo/pna/service/LinkServiceIntegrationTests.java
+++ src/test/java/dev/pulceo/pna/service/LinkServiceIntegrationTests.java
@@ -3,9 +3,15 @@ package dev.pulceo.pna.service;
 import dev.pulceo.pna.exception.JobServiceException;
 import dev.pulceo.pna.exception.LinkServiceException;
 import dev.pulceo.pna.model.ResourceType;
+import dev.pulceo.pna.model.iperf3.IperfClientProtocol;
+import dev.pulceo.pna.model.iperf3.IperfRequest;
+import dev.pulceo.pna.model.jobs.IperfJob;
+import dev.pulceo.pna.model.jobs.NpingJob;
 import dev.pulceo.pna.model.jobs.PingJob;
 import dev.pulceo.pna.model.link.Link;
 import dev.pulceo.pna.model.node.Node;
+import dev.pulceo.pna.model.nping.NpingClientProtocol;
+import dev.pulceo.pna.model.nping.NpingRequest;
 import dev.pulceo.pna.model.ping.IPVersion;
 import dev.pulceo.pna.model.ping.PingRequest;
 import org.junit.jupiter.api.Test;
@@ -110,6 +116,62 @@ public class LinkServiceIntegrationTests {
 
         // when
         this.linkService.addJobToLink(linkId, pingJobId);
+
+        // then
+        Optional<Link> updatedLink = this.linkService.readLink(linkId);
+        assertTrue(updatedLink.isPresent());
+        Link actualLink = updatedLink.get();
+        assertEquals(testLink1.getName(), actualLink.getName());
+        assertEquals(testLink1.getResourceType(), actualLink.getResourceType());
+        assertEquals(testLink1.getLinkDirectionType(), actualLink.getLinkDirectionType());
+        assertEquals(testLink1.getSrcId(), actualLink.getSrcId());
+        assertEquals(testLink1.getDestId(), actualLink.getDestId());
+        assertFalse(updatedLink.get().getJobs().isEmpty());
+    }
+
+    @Test
+    // only the associated jobs are loaded from the db, PingRequest is not loaded
+    public void testAddNPingJobToExistingLink() throws LinkServiceException, JobServiceException {
+        // given
+        long firstSrcNodeId = nodeService.createNode(new Node("testSrcNode1", "Germany", "Bamberg"));
+        long firstDestNodeId = nodeService.createNode(new Node("testDestNode1", "Germany", "Bamberg"));
+        Link testLink1 = new Link("testLink", ResourceType.NODE, firstSrcNodeId, firstDestNodeId);
+        long linkId = this.linkService.createLink(testLink1);
+
+        NpingRequest npingRequest = new NpingRequest("localhost", "localhost", 4002, NpingClientProtocol.TCP, 1, "lo");
+        NpingJob npingJob = new NpingJob(npingRequest, 15);
+        long npingJobId = this.jobService.createNpingJob(npingJob);
+
+        // when
+        this.linkService.addJobToLink(linkId, npingJobId);
+
+        // then
+        Optional<Link> updatedLink = this.linkService.readLink(linkId);
+        assertTrue(updatedLink.isPresent());
+        Link actualLink = updatedLink.get();
+        assertEquals(testLink1.getName(), actualLink.getName());
+        assertEquals(testLink1.getResourceType(), actualLink.getResourceType());
+        assertEquals(testLink1.getLinkDirectionType(), actualLink.getLinkDirectionType());
+        assertEquals(testLink1.getSrcId(), actualLink.getSrcId());
+        assertEquals(testLink1.getDestId(), actualLink.getDestId());
+        assertFalse(updatedLink.get().getJobs().isEmpty());
+    }
+
+    @Test
+    // only the associated jobs are loaded from the db, PingRequest is not loaded
+    public void testAddIperfJobToExistingLink() throws LinkServiceException, JobServiceException {
+        // given
+        long firstSrcNodeId = nodeService.createNode(new Node("testSrcNode1", "Germany", "Bamberg"));
+        long firstDestNodeId = nodeService.createNode(new Node("testDestNode1", "Germany", "Bamberg"));
+        Link testLink1 = new Link("testLink", ResourceType.NODE, firstSrcNodeId, firstDestNodeId);
+        long linkId = this.linkService.createLink(testLink1);
+
+        IperfRequest iperfRequest = new IperfRequest("localhost", "localhost", 5001, 0, 1, IperfClientProtocol.TCP, "lo");
+        IperfJob iperfJob = new IperfJob(iperfRequest, 15);
+        long iperfJobId = this.jobService.createIperfJob(iperfJob);
+
+        // when
+        this.linkService.addJobToLink(linkId, iperfJobId);
 
         // then
         Optional<Link> updatedLink = this.linkService.readLink(linkId);

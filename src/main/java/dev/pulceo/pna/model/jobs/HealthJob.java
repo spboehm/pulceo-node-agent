d123a0179ef84b88dc3d98e7e2d7d40b20811fde
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-11-14 12:14:51 +0100

    add HealthJob

---
+++ src/main/java/dev/pulceo/pna/model/jobs/HealthJob.java
@@ -0,0 +1,16 @@
+package dev.pulceo.pna.model.jobs;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class HealthJob extends NodeJob {
+
+
+
+}

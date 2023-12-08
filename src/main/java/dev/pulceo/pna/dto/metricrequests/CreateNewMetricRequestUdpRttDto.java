10c723398b47bc22141d89a8165d9bd9d99eae53
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-12-08 14:57:33 +0100

    add further dto classes for metrics

---
+++ src/main/java/dev/pulceo/pna/dto/metricrequests/CreateNewMetricRequestUdpRttDto.java
@@ -0,0 +1,20 @@
+package dev.pulceo.pna.dto.metricrequests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateNewMetricRequestUdpRttDto {
+    private String type;
+    private String recurrence;
+    private boolean enabled;
+    // TODO: ipVersion
+    @Builder.Default
+    private int rounds = 10;
+}
--- src/main/java/dev/pulceo/pna/service/LinkService.java
+++ src/main/java/dev/pulceo/pna/service/LinkService.java
@@ -81,4 +81,6 @@ public class LinkService {
         return false;
     }
 
+
+
 }

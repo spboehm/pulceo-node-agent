a71f94a78d62dbbed13d9ee371c4a68625104743
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-12-21 11:07:54 +0100

    update ResourceUtilizationService

--- src/main/java/dev/pulceo/pna/service/ResourceUtilizationService.java
+++ src/main/java/dev/pulceo/pna/service/ResourceUtilizationService.java
@@ -1,6 +1,15 @@
 package dev.pulceo.pna.service;
 
-public class ResourceService {
+public class ResourceUtilizationService {
+
+
+    // CPU utilization of node
+    // node
+    // TS
+    // value
+    // unit
+
+
 
     // TODO: statically available resources (CPU, memory, disk, network, etc.) on Nodes and their replicates
 
---
+++ src/test/java/dev/pulceo/pna/service/ResourceServiceUnitTests.java
@@ -0,0 +1,5 @@
+package dev.pulceo.pna.service;
+
+public class ResourceServiceUnitTests {
+
+}

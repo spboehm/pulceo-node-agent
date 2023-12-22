b42d1944c73f8c39a287b70ebdad6ef1b9086e38
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2023-12-22 11:37:31 +0100

    update OSResourceUtilService

--- src/main/java/dev/pulceo/pna/service/OSResourceUtilizationService.java
+++ src/main/java/dev/pulceo/pna/service/OSResourceUtilizationService.java
@@ -2,17 +2,34 @@ package dev.pulceo.pna.service;
 
 public class ResourceUtilizationService {
 
+    /* Important mount /proc of host to container
+     * CPU utilization
+     */
 
     // CPU utilization of node
+    //
     // node
     // TS
     // value
     // unit
 
+    /* Memory utilization  */
+    // /proc/meminfo
+    //
+
+    /* Disk */
+    // free capacity
+    
 
 
     // TODO: statically available resources (CPU, memory, disk, network, etc.) on Nodes and their replicates
 
     // TODO: totally available resources (CPU, memory, disk, network, etc.) on Nodes and their replicates
 
+
+    //
+
+    // tx rx
+    // ip -s -j -p link show eth0
+    // /sys/class/net/eth0/statistics
 }

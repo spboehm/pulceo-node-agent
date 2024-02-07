f5891bb89c1a978ffb00e40a2254bf42ba6d27c9
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-02-07 22:25:22 +0100

    add cat-proc-cpuinfo.txt

---
+++ src/main/java/dev/pulceo/pna/util/CPUUtil.java
@@ -0,0 +1,7 @@
+package dev.pulceo.pna.util;
+
+public class CPUUtil {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}

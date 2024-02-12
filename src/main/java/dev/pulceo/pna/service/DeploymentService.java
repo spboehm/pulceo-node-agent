42211facc7fc67f11f4c2a5430e57eae064055a1
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-02-12 18:17:51 +0100

    adjust gitignore

--- src/main/java/dev/pulceo/pna/service/DeploymentService.java
+++ src/main/java/dev/pulceo/pna/service/DeploymentService.java
@@ -8,16 +8,22 @@ import io.kubernetes.client.openapi.models.V1Service;
 import io.kubernetes.client.openapi.models.V1ServiceList;
 import io.kubernetes.client.util.Config;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 @Service
 public class DeploymentService {
 
+    @Value("${k3s.config.path}")
+    private String k3sConfigPath;
+
     @PostConstruct
     private void init() throws IOException, ApiException {
         try {
-            ApiClient client = Config.fromConfig("/k3s.yaml");
+            File file = new File(k3sConfigPath);
+            ApiClient client = Config.fromConfig(k3sConfigPath);
             Configuration.setDefaultApiClient(client);
 
             CoreV1Api api = new CoreV1Api();

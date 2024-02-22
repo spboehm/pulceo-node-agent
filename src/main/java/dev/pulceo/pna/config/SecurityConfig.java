d9fa3843e62e0e91d0b1fb3e1428d38359195332
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-02-22 21:18:37 +0100

    revise SecurityConfig

--- src/main/java/dev/pulceo/pna/config/SecurityConfig.java
+++ src/main/java/dev/pulceo/pna/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package dev.pulceo.pna.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +12,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                .anyRequest()
+                                .authenticated());
         return http.build();
     }
 

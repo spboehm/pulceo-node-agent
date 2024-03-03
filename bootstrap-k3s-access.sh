38163ac5251956d9e09f23fb0613cba171009b9c
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-03-03 17:40:49 +0100

    fix cluster naming

--- bootstrap-k3s-access.sh
+++ bootstrap-k3s-access.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl config use-context pna-test
+kubectl config use-context k3d-pna-test
 mkdir -p .k3s-service-account
 kubectl get secret k3s-serving -n kube-system -o jsonpath="{['data']['tls\.crt']}" | base64 --decode | tail -n 10 > .k3s-service-account/ca.crt
 kubectl create serviceaccount cluster-admin

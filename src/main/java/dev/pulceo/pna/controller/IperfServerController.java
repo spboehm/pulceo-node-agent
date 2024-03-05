2b237c5bc97978f9822d1ccee533b341e4c25105
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-03-05 23:43:44 +0100

    add deletion endpoints

--- src/main/java/dev/pulceo/pna/controller/IperfServerController.java
+++ src/main/java/dev/pulceo/pna/controller/IperfServerController.java
@@ -3,10 +3,9 @@ package dev.pulceo.pna.controller;
 import dev.pulceo.pna.exception.NodeServiceException;
 import dev.pulceo.pna.service.IperfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/iperf3-servers")
@@ -25,6 +24,16 @@ public class IperfServerController {
             return ResponseEntity.ok().body(this.iperfService.startIperf3ServerAndReturnPort());
         } catch (Exception e) {
             throw new NodeServiceException("Could not start Iperf3 server process!", e);
+        }
+    }
+
+    @DeleteMapping("/{port}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteIperf3ServerByPort(@PathVariable int port) throws NodeServiceException {
+        try {
+            this.iperfService.stopIperf3Server(port);
+        } catch (Exception e) {
+            throw new NodeServiceException("Could not stop Iperf3 server process!", e);
         }
     }
 }

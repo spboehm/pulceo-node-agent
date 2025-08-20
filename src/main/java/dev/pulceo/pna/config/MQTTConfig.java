71b5c1406d926e29a70090706d1f3478994c530b
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2025-08-20 08:43:14 +0000

    add ingoing MQTT config

--- src/main/java/dev/pulceo/pna/config/MQTTConfig.java
+++ src/main/java/dev/pulceo/pna/config/MQTTConfig.java
@@ -6,21 +6,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Configuration
 public class MQTTConfig {
 
     @Value("${pna.uuid}")
-    private String deviceId;
+    private String pnaUUID;
 
     @Value("${pna.mqtt.client.id}")
     private String mqttClientId;
@@ -33,6 +38,37 @@ public class MQTTConfig {
     private String mqttBrokerPassword;
 
     /* Inbound */
+    @Bean
+    public BlockingQueue<Message<?>> mqttBlockingQueueTask() { return new LinkedBlockingQueue<>(); }
+
+    @Bean
+    public MessageChannel mqttInputChannelForTask() { return new DirectChannel(); }
+
+    @Bean
+    public MessageProducer inboundTask() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(UUID.randomUUID().toString(), mqttClientFactory(),
+                        "dt/" + this.pnaUUID + "/tasks");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.connectComplete(true);
+        adapter.setOutputChannel(mqttInputChannelForTask());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannelForTask")
+    public MessageHandler handler() {
+        return message -> {
+            try {
+                this.mqttBlockingQueueTask().put(message);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
 //    @Bean
 //    public MessageChannel mqttInputChannel() {
 //        return new DirectChannel();
@@ -100,7 +136,6 @@ public class MQTTConfig {
     public MessageChannel mqttOutboundChannel() {
         return new DirectChannel();
     }
-
 
     @Bean
     @ServiceActivator(inputChannel = "mqttOutboundChannel")
